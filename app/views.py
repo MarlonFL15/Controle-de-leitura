0bd8ba95c0d2460765f6b429451b5b8e1da93d84
unknown <marlonfleite50@gmail.com>
2021-10-29 17:21:00 -0400

    implementação da tela de tabela de leituras

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'leituras/index.html')
+def leituras(request):
+    return render(request, 'leituras/leituras.html')
